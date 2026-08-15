cfebc574f943e7e7e21cd5c235571c298077d280
Mike Penz <opensource@mikepenz.dev>
2026-08-15 10:19:40 +0200

    fix: accept comma separated exclude_sources again
    exclude_sources was documented as comma separated, but has been parsed
    via getMultilineInput (newlines only) since the multi report refactor in
    27566d1. Any comma separated value ended up as a single unmatchable
    entry, silently excluding nothing.
    
    Now both comma and newline separated values are accepted.
    
    Fixes #1606

--- __tests__/utils.test.ts
+++ __tests__/utils.test.ts
@@ -1,9 +1,27 @@
-import {buildTable, readTransformers} from '../src/utils.js'
+import {buildTable, readTransformers, splitList} from '../src/utils.js'
 import {describe, expect, it} from 'vitest'
 
 /**
  *   Copyright 2024 Mike Penz
  */
+
+describe('splitList', () => {
+  it('should split comma separated values', async () => {
+    expect(splitList(['/build/,/__pycache__/'])).toStrictEqual(['/build/', '/__pycache__/'])
+  })
+
+  it('should keep newline separated values', async () => {
+    expect(splitList(['/build/', '/__pycache__/'])).toStrictEqual(['/build/', '/__pycache__/'])
+  })
+
+  it('should handle mixed separators, whitespace and empty entries', async () => {
+    expect(splitList([' /build/ , ', '', '/node_modules/,'])).toStrictEqual(['/build/', '/node_modules/'])
+  })
+
+  it('should return an empty array for empty input', async () => {
+    expect(splitList([])).toStrictEqual([])
+  })
+})
 
 describe('readTransformers', () => {
   it('should successfully parse default transformer', async () => {
@@ -37,8 +55,14 @@ describe('readTransformers', () => {
 
 describe('buildTable', () => {
   it('should close empty cells', async () => {
-    expect(buildTable([[{data: '', header: true}, {data: 'Tests', header: true}], ['', 'A']])).toStrictEqual(
-      '<table><tr><th></th><th>Tests</th></tr><tr><td></td><td>A</td></tr></table>'
-    )
+    expect(
+      buildTable([
+        [
+          {data: '', header: true},
+          {data: 'Tests', header: true}
+        ],
+        ['', 'A']
+      ])
+    ).toStrictEqual('<table><tr><th></th><th>Tests</th></tr><tr><td></td><td>A</td></tr></table>')
   })
 })
